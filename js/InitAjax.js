2ed9ccda2c37faeeebd113edff7cd056de11a39d
lamine <soniamokaddem88@gmail.com>
2021-01-22 12:25:09 +0100

    ajout eval

---
+++ js/InitAjax.js
@@ -0,0 +1,13 @@
+const xhr = new XMLHttpRequest();
+xhr.addEventListener("readystatechange", function(){
+
+    if(xhr.readyState===4 && xhr.status===200){
+        console.log(xhr.response);
+        const menu = JSON.parse(xhr.response);
+        for(let elt of menu)
+    }
+});
+
+xhr.open("get","data/menu.json", true);
+
+xhr.send();
--- js/dojosjs.js
+++ js/dojosjs.js
@@ -1,1 +1,39 @@
 console.log("Go Chuck !");
+document.querySelector("div").onmouseover = function() {mouseOver()};
+document.querySelector("div").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+    document.querySelector("div").style.borderRadius = "0%";
+}
+
+function mouseOut() {
+    document.querySelector("div").style.borderRadius = "50%";
+}
+
+let fleche = document.querySelectorAll("header");
+let i;
+for (i = 0; i < fleche.length; i++) {
+    fleche[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.visibility === "hidden" || content.style.visibility === "" )
+         {
+            content.style.visibility = "visible"; 
+            content.style.height = "auto";
+        }  else {
+            content.style.visibility = "hidden";
+            content.style.height = "0px"
+        }
+
+    });
+}
+class Principale {
+    constructor( nom, lien, sousmenus){
+        this.nom = nom;
+        this.lien = lien;
+        this.sousmenus = sousmenus;
+}
+getHtml = () =>{
+    const elt = 
+}
+}
